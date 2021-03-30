836be9c3c53c353b807d1781386c31218acbc451
spr15bd <bevandady@gmail.com>
2021-03-30 22:25:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -586,7 +586,7 @@ class Main extends React.Component {
 					if (y>0) {
 						var z=Math.floor(Math.random()*y);
 						console.log("This in doComputerAI() refers to "+this);
-						takeSquare(emptySquares[z]);
+						this.Main.takeSquare(emptySquares[z]);
 						// if no corners or central positions are available, take one of the remaining four positions randomly
 					} else {
 						var emptySquares=[];
