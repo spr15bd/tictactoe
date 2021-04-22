5707e950ab7c7e90a07e1088a4ab8938d76a8335
spr15bd <bevandady@gmail.com>
2021-04-22 21:25:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -655,8 +655,8 @@ class Main extends React.Component {
 		//		check for winner
 		//		if no win minimax on other player
 				
-		var result=this.turn==this.state.playerOne?100:-100;
-		var latestResult=this.turn==this.state.playerOne?100:-100;
+		var result=turn==this.state.playerOne?100:-100;
+		var latestResult=turn==this.state.playerOne?100:-100;
 		var bestSquare=0;
 		var newTurn;
 		console.log("new minimax on player "+turn+", depth is "+depth+", boardState: "+this.state.boardState);
